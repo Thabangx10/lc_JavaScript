a025acf140fc54c862efe6249d453f6d2780e0a2
Thabangx10 <Thabangx12@gmail.com>
2023-05-15 21:32:57 +0200

    school

---
+++ script/home.js
@@ -0,0 +1,24 @@
+// Define the variables and concatenate them: In the script.js file, define the variables and concatenate them using the + operator.
+
+let firstName = 'Thabang';
+let lastName = 'Kganana';
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// Updating the content in our <p></p>, using its id.
+
+document.getElementById('h1').innerHTML = fullName;
+// console.log();
+
+
+// Another variable
+
+numberVar = 5
+console.log(numberVar * 5);
+
+// "Const" var
+
+// const name = 'thabang'
+// const name = 'Kganana'
+
+// -> We get an error due to us redaclring the same variable more than once (a blocked scoped variable)
