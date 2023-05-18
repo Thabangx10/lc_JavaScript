4581c36b6ef6a4169debe0743c85e715a5bb5520
Thabangx10 <Thabangx12@gmail.com>
2023-05-18 02:58:32 +0200

    LC

--- objectsAndarrays/script/code.js
+++ objectsAndarrays/script/code.js
@@ -1,16 +1,30 @@
-ages = {
-    num1: 23,
-    num2: 20,
-    num3: 20
-};
+// OBJECTS
 
-persons = {
-    Person1:'Thabang',
-    Person2:'Summer',
-    Person3:'Rabia'};
+// ages = {
+//     num1: 23,
+//     num2: 20,
+//     num3: 20
+// };
+
+// persons = {
+//     Person1:'Thabang',
+//     Person2:'Summer',
+//     Person3:'Rabia'};
 
 // let access = Object.values(access)[0]
-console.log(typeof persons);
+// console.log(persons.Person1);
+// console.log(persons.Person2);
+// console.log(persons.Person3);
+
+//ARRAY
+
+let ages = [23,20,20];
+let persons = ['Thabang', 'Summer', 'Rabia'];
+
+console.log(ages[0]);
+console.log(persons[2]);
+
+
 
 
 
