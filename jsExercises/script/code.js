fa4c15eeb17092485fb1cb851c377cc0a6e61960
Thabangx10 <Thabangx12@gmail.com>
2023-05-26 08:24:59 +0200

    'change'

---
+++ jsExercises/script/code.js
@@ -0,0 +1,39 @@
+// How to create a function called fullName that returns your full name and displays it on the console
+
+let name1 = 'Thabang Kganana'
+
+function fullname(name1) {
+    console.log(name1);
+}
+
+fullname(name1)
+
+
+// Question 2
+
+// Please create the following variables to store your information:
+// firstName, lastName, age, list of subjects, and address, which will store the street
+// name, street number, suburb, city, and country name.
+
+let student = {
+    firstname : 'Thabang',
+    lastname : 'Kganana',
+    Age : 24,
+};
+
+console.log(student.firstname, student.lastname, student.Age);
+
+
+let subjects = ['JavaScript', 'HTML', 'CSS/BOOTSRTRAP']
+
+console.log(subjects[0], subjects[1], subjects[2]);
+
+let home = {
+    Address : 'YMCA, springbok rd',
+    streetNumber : 16,
+    suburb : 'Kewtown',
+    city : 'Athlone',
+    country : 'South Africa'
+}
+
+console.log(home.Address, home.streetNumber, home.suburb, home.city, home.country);
